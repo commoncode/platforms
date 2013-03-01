3bc7c0c82689f5fb5fc9bea4793e9489ff939321
mark hellewell <mark.hellewell@icloud.com>
2013-03-01 13:38:57 +1100

    simple debug logging
    Signed-off-by: mark hellewell <mark.hellewell@icloud.com>

--- platforms/middleware.py
+++ platforms/middleware.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import logging
+logger = logging.getLogger(__name__)
+
 from django.conf import settings
 from django.core.exceptions import MiddlewareNotUsed
 
@@ -8,12 +11,14 @@ class PlatformResolutionMiddleware(object):
     """
     def __init__(self):
         """This middleware must be enabled, as well as included in
-        INSTALLED_MIDDLEWARE"""
-
+        MIDDLEWARE_CLASSES
+        """
         if not getattr(settings, 'PLATFORMS_USE_PLATFORMS', False):
+            logger.debug('PLATFORMS_USE_PLATFORMS setting must be True to use this middleware')
             raise MiddlewareNotUsed
 
     def process_request(self, request):
+        logger.debug('PlatformResolutionMiddleware.process_request entered')
         return None
 
     # will other methods be useful? process_view,
