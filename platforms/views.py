03e74c027f1a74ac1d0cfe2b0711a6c77df24633
Adam Hoddinott <adam@messymind.net>
2013-03-06 11:42:38 +1100

    Added view listview and detailview mixins for filtering queries

--- platforms/views.py
+++ platforms/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+
+class PlatformListMixin(object):
+    """ Override get_queryset to make use of platforms if it is
+    installed to filter the results
+    """
+    def get_queryset(self):
+        if hasattr(self.model.objects, "platform"):
+            return self.model.objects.platform(self.request.platform)
+        else:
+            return super(PlatformListMixin, self).get_queryset()
+
+
+class PlatformDetailMixin(object):
+    """ Override get_queryset to make use of platforms if it is
+    installed to filter the results
+    """
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        if hasattr(self.model.objects, "platform") and slug:        
+            return self.model.objects.platform(self.request.platform).filter(
+                slug=slug
+            )
+        else:
+            return super(PlatformDetailMixin, self).get_queryset()
