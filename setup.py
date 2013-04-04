e8c03e213fedbb5ed9789cece3ee8f779095b624
Adam Hoddinott <adam@messymind.net>
2013-04-05 10:31:37 +1100

    Bumping version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup( name='platforms',
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Platforms for Django',
     author = 'Mark Hellewell',
     author_email = 'mark.hellewell@commoncode.com.au',
