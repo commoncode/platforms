ff983a44a9fe8fedce17d860035bfcc969a410fe
Daryl Antony <daryl@commoncode.com.au>
2014-06-01 14:19:20 +1000

    ignore

--- platforms/models.py
+++ platforms/models.py
@@ -1,57 +1,44 @@
 # -*- coding: utf-8 -*-
-from entropy.base import OrderingMixin, SlugMixin, TitleMixin, AttributeMixin
-
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
+from entropy.base import OrderingMixin, SlugMixin, TitleMixin, AttributeMixin
+
 
 class Platform(TitleMixin, SlugMixin, AttributeMixin):
-    """A platform.
-    """
-    # title and short_title from TitleMixin
-    # slug from SlugMixin
+    '''
+    Platform.
 
-    # TODO add 'canonical' FK relationship to canonical Platform.
+    '''
+    # title
+    # short_title
+    # slug
+
     pass
 
 
-class PlatformObject(models.Model):
-    """A Generic relation between objects and Platforms.
-    """
-    platform = models.ForeignKey(Platform)
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey()
-
-    def __unicode__(self):
-        return self.platform.title
-
-
-class PlatformObjectManager(models.Manager):
-    """An object manager to be used by publishable objects.
-    """
-    def platform(self, platform):
-        """Returns a QuerySet for the base Model, limited to ids of
-        instances related to the supplied Platform.
-        """
-        ctype = ContentType.objects.get_for_model(self.model)
-        return self.get_query_set().filter(
-            id__in=PlatformObject.objects.filter(
-                platform=platform,
-                content_type=ctype
-            ).values_list('object_id', flat=True))
-
 
 class Resolution(OrderingMixin):
-    """Different ways we might resolve to a Platform instance.
-    """
-    # order from OrderingMixin
-    platform = models.ForeignKey(Platform)
-    domain = models.CharField(max_length=255, blank=True,
+    '''
+    If Platforms are used in a Request / Response context, we can use Resolutions
+    to determine which Platform we're dealing with.
+
+    '''
+
+    # order
+
+    platform = models.ForeignKey('Platform')
+    domain = models.CharField(
+        max_length=256,
+        blank=True,
         help_text="Start with . to match subdomains e.g. .domain.com to match this.domain.com and www.domain.com")
-    subdomain = models.CharField(max_length=255, blank=True)
-    uripattern = models.CharField(max_length=255, blank=True)
+    subdomain = models.CharField(
+        max_length=256,
+        blank=True)
+    uripattern = models.CharField(
+        max_length=256,
+        blank=True)
 
     def __unicode__(self):
         return "domain: {}, order: {}".format(self.domain, self.order)
